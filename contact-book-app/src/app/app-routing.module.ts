9f00933fc0462cad0d5e2b520b4a94dc2eeca4e6
HannahBloyd <hannahbloyd15@gmail.com>
2020-11-09 21:20:25 -0500

    start routing

--- contact-book-app/src/app/app-routing.module.ts
+++ contact-book-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactDirectoryComponent } from './contact-directory/contact-directory.component';
+import { ContactDisplayComponent } from './contact-display/contact-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'contacts', component: ContactDirectoryComponent},
+  {path : 'contact/:firstName/:lastName', component: ContactDisplayComponent},
+  { path: '',
+		redirectTo: '/contacts', 
+		pathMatch: 'full'
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
